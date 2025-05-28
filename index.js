25b1568afddcf78a73599b19806a2fa3df5cecaf
uamii <umayarhossainashik48889@gmail.com>
2025-05-28 22:36:34 +0600

    origing wooden spice

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const corsOptions = {
   origin: [
     'http://localhost:5173',
     'http://localhost:5174',
-    'https://signin-signup-firebase-febe3.web.app/',
+    'https://woodenspice-mnkgnj-beutha-umai.web.app',
   ],
   credentials: true,
   optionSuccessStatus: 200,
